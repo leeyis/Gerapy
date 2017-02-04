05dffc3556731fc180add425e43895f53e52aa2a
Germey <cqc@cuiqingcai.com>
2017-02-04 22:57:43 +0800

    init layout

--- gerapy/webui/core/views.py
+++ gerapy/webui/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+from .models import Spider
+
+
+def index(request):
+    latest_question_list = Spider.objects.all()
+    return render(request, 'pages/index.html')
--- gerapy/webui/webui/urls.py
+++ gerapy/webui/webui/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('core.urls')),
+
 ]
